0bad2f850dd80f22bdc59d9331688300d755c35c
Marcos Sanda Fdez <marcos92sanda@gmail.com>
2023-04-22 20:00:03 +0200

    Actualizaciones v2.0

--- demo/src/main/java/com/edix/tfg/cazadoresLibros/Dao/TarjetaDao.java
+++ demo/src/main/java/com/edix/tfg/cazadoresLibros/Dao/TarjetaDao.java
@@ -1,5 +1,16 @@
 package com.edix.tfg.cazadoresLibros.Dao;
 
+import java.util.List;
+
+import com.edix.tfg.cazadoresLibros.beans.Tarjeta;
+
 public interface TarjetaDao {
+	
+	//Método que lista tarjetas
+		List<Tarjeta> findAll();
+		//Método que registra tarjetas 
+		int registrarTarjeta (Tarjeta tarjeta);
+		//Método que elimina tarjetas
+		int eliminarTarjeta (int idTarjeta);
 
 }
--- demo/src/main/java/com/edix/tfg/cazadoresLibros/Dao/TarjetaDaoImplMy8.java
+++ demo/src/main/java/com/edix/tfg/cazadoresLibros/Dao/TarjetaDaoImplMy8.java
@@ -1,5 +1,51 @@
 package com.edix.tfg.cazadoresLibros.Dao;
+	
+	import java.util.List;
 
-public class TarjetaDaoImplMy8 {
+	import org.springframework.beans.factory.annotation.Autowired;
 
-}
+	import com.edix.tfg.cazadoresLibros.beans.Tarjeta;
+	import com.edix.tfg.cazadoresLibros.repository.TarjetaRepository;
+
+	public class TarjetaDaoImplMy8 implements TarjetaDao {
+		
+		@Autowired
+		TarjetaRepository trepo;
+		
+		// Método para listar las tarjetas 
+		@Override
+		public List<Tarjeta> findAll() {
+			return trepo.findAll();
+		}
+		
+		// Método para registrar una tarjeta
+		@Override
+		public int registrarTarjeta(Tarjeta tarjeta) {
+			
+			int filas = 0;
+			try {
+				trepo.save(tarjeta);
+				filas =1;
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			return filas;
+			
+		}
+		
+		// Método para eliminar una tarjeta
+		@Override
+		public int eliminarTarjeta(int idTarjeta) {
+			int filas = 0;
+			try {
+				trepo.deleteById(idTarjeta);
+				filas = 1;
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			return filas;
+		}
+
+	}
